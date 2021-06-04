8c1238cedbb8b9b400221ca031649e1f0865f229
camilocruz10 <cruzcamilo@javeriana.edu.co>
2021-06-03 22:26:07 -0500

    menu ready

--- Assets/Scripts/Player_movement.cs
+++ Assets/Scripts/Player_movement.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_movement : MonoBehaviour
 {
+    public bool start = false;
+    public bool end = false;
     public float Speed = 8.0f;
     public float Rotation_Speed = 64.0f;
     public float JumpForce = 1.0f;
-
+    public GameObject startObject;
+    public GameObject endObject;
     private Rigidbody Physics;
     
     // Start is called before the first frame update
@@ -15,7 +19,7 @@ public class Player_movement : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
+        
         Physics = GetComponent<Rigidbody>();
         
     }
@@ -23,21 +27,57 @@ public class Player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // movimiento
-        float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(hor, 0.0f, ver) * Time.deltaTime * Speed);
-
-        //rotación
-        float rotationY = Input.GetAxis("Mouse X");
-        
-        transform.Rotate(new Vector3(0, rotationY * Time.deltaTime * Rotation_Speed, 0));
-
-        //salto
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Physics.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+        if (!start && !end) {
+            startObject.SetActive(true);
+            endObject.SetActive(false);
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                start = true;
+            }
         }
+        else if (end) {
+            startObject.SetActive(false);
+            endObject.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        else {
+            startObject.SetActive(false);
+            endObject.SetActive(false);
+            // movimiento
+            float hor = Input.GetAxis("Horizontal");
+            float ver = Input.GetAxis("Vertical");
+
+            transform.Translate(new Vector3(hor, 0.0f, ver) * Time.deltaTime * Speed);
+
+            //rotación
+            float rotationY = Input.GetAxis("Mouse X");
+            
+            transform.Rotate(new Vector3(0, rotationY * Time.deltaTime * Rotation_Speed, 0));
+
+            //salto
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Physics.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+            }
+
+            //juego termina
+            if (Input.GetKeyDown(KeyCode.E)) {
+                end = true;
+            }
+        }
+
+    }
+    public void botonInicio() {
+        if (!start && !end) {
+            start = true;
+        }
+        else if (end) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+    public void botonPrueba() {
+        
     }
 }
